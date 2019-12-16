d2aef7d6c90c3d5a8ce773a211a99310e1c380a3
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-16 14:17:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
     end 
-  end.flatten.compact
+  end
   
   all_holidays_with_bbq(holiday_supplies)
   
