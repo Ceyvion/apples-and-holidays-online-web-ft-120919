96cef5ab4ce65a058cebf743b64ae5db7f6767d1
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-16 13:55:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash.each do |holiday_hash, item|
     holiday_hash[:winter].each do 
-      
+    item << decor
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
